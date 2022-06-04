bc045cd7a9355e7616da9549a18cf1f8c5ad59b5
Sasha <aleksandr.marfut@gmail.com>
2022-06-04 11:48:50 +0200

    divide_by_zero_check

--- Math.cs
+++ Math.cs
@@ -2,6 +2,11 @@ public class Math
 {
     public static int Divide(int a, int b)
     {
+        if (b == 0)
+        {
+            throw new DivideByZeroException("You cannot divide by zero today.");
+        }
+        
         return a / b;
     }
 }
